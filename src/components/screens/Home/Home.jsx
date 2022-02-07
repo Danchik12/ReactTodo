00517feceb6f62fce7ea07068331bbeebcc9782c
Dmitry Petrukovich <qwerty1111311@gmail.com>
2022-02-07 08:08:08 +0300

    Filter add

--- src/components/screens/Home/Home.jsx
+++ src/components/screens/Home/Home.jsx
@@ -1,9 +1,33 @@
-import React, {  useState } from 'react' 
+import React, {  useState,useEffect } from 'react' 
 import TaskItem from './item/TaskItem'
 import CreateTaskField from './item/CreateTaskField'
 const data=[]
+
+
 const Home = () => {
 	const [tasks,setTasks]= useState(data)
+	const [filtered,setFiltered]=useState(tasks)
+
+	useEffect(() => {
+		if (localStorage.getItem('example')){
+			setTasks(JSON.parse(localStorage.getItem('example')))
+		}
+
+	},[])
+
+	useEffect (() =>{
+	localStorage.setItem('example',JSON.stringify(tasks))
+
+	},[tasks])
+
+
+	
+	
+	useEffect (() =>{
+setFiltered(tasks)
+
+
+	},[tasks])
 
 	const changeTask = id => {
 		const copy =[...tasks]
@@ -14,19 +38,39 @@ const Home = () => {
 
 	const removeTask = id => {
 		setTasks([...tasks].filter(t =>t._id !== id))
+
 	}
 
+	const filteredTask = isCompleted => {
+		if (isCompleted ==="all"){
+			setFiltered(tasks)
+		}else {
+			let newTasks=[...tasks].filter(item => item.isCompleted === isCompleted)
+			setFiltered(newTasks)
+		}
+
+	}
 	
 return (
 <div className=' text-white w-4/5 mx-auto'>
 <h1 className='text-2xl font-bold text-center mb-8'>React Todo App</h1>
-{tasks.map(task => 
+<div  className='text-center'> 
+<button className='w-[70px] h-[35px] bg-gray-800 mr-4 border-2 rounded-full hover:border-red-600 
+hover:text-red-600 transition-colors ease-in-out duration-300' onClick={() => filteredTask('all')}>All</button>
+<button  className='w-[70px] h-[35px] bg-gray-800  mr-4 border-2 rounded-full hover:border-red-600 
+hover:text-red-600 transition-colors ease-in-out duration-300' onClick={() => filteredTask(false)}>Open</button>
+<button className='w-[70px] h-[35px] bg-gray-800 border-2 rounded-full hover:border-red-600 
+hover:text-red-600 transition-colors ease-in-out duration-300' onClick={() => filteredTask(true)}>Clossed</button>
+</div>
+<CreateTaskField setTasks={setTasks}/>
+{filtered.map(task =>
+
 	<TaskItem 
 	key={task._id} 
 	task={task} 
 	changeTask={changeTask} 
 	removeTask={removeTask}/>)}
-<CreateTaskField setTasks={setTasks}/>
+
 </div>
 )	
 }
--- src/components/screens/Home/item/CreateTaskField.jsx
+++ src/components/screens/Home/item/CreateTaskField.jsx
@@ -12,6 +12,7 @@ const [title,setTitle]=useState('')
 		},
 		])
 		setTitle('')
+		
 
 	}
 
