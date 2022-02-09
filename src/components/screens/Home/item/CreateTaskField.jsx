61086eea3b0da5d280f7e66b0a9fb3e766275a6a
Dmitry Petrukovich <qwerty1111311@gmail.com>
2022-02-09 07:10:25 +0300

    Add files via upload

--- src/components/screens/Home/item/CreateTaskField.jsx
+++ src/components/screens/Home/item/CreateTaskField.jsx
@@ -1,8 +1,17 @@
 import React, {  useState } from 'react'
 
+
 const CreateTaskField = ({setTasks}) => {
 const [title,setTitle]=useState('')
-	const addTask = (title) => {
+const [titleDirty,setTitleDirty]=useState(false)
+const [error,setError]=useState('Строка не должна быть пустой!')
+	const validateTask = (title) => {
+		if (title === ''){
+			setTitleDirty(true)
+			setError('Строка не должна быть пустой!')
+		}
+		else{
+		setError('')
 		setTasks (prev =>[...prev,
 			{
 			_id:new Date(),
@@ -12,20 +21,25 @@ const [title,setTitle]=useState('')
 		},
 		])
 		setTitle('')
-		
+	}	
 
 	}
 
 return (
+	<>
+{(titleDirty && error) && <div style={{color:'red'}}>{error}</div> }
+	
 <div className='flex items-center  justify-between mb-4 rounded-2xl border-gray-800 
-px-5 py-3 border-2 w-full mt-20'>
+px-5 py-3 border-2 w-full mt-10'>
 <input type='text'className='bg-transparent w-full border-none'  
-onChange={e => setTitle(e.target.value) } 
+onChange={e => setTitle( e.target.value) } 
+
 value={title}
-onKeyPress={e => e.key === 'Enter' && addTask(title)}
+onKeyPress={e => e.key === 'Enter'  && validateTask(title) }
 placeholder="Add a task"
 />
 </div>
+</>
 )	
 }
 
